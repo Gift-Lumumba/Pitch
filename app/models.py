86aeeaa3253a1eb7d0d393e8ce331d725f3d086b
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-06 21:22:33 +0300

    added ForeignKey to connect users and their comments

--- app/models.py
+++ app/models.py
@@ -44,6 +44,7 @@ class User(db.Model):
   __tablename__ = 'users'
   id = db.Column(db.Integer,primary_key = True)
   username = db.Column(db.String(255))
+  comment_id = db.Column(db.Integer,db.ForeignKey('comments.id'))
 
   def __repr__(self):
     return f'User{self.username}'
