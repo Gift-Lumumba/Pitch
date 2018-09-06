946c67ccacbe28dea49b6ef2894b345a05f3e75f
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-06 12:54:21 +0300

    added route with pitch() view function responding to pitch.html file

--- app/views.py
+++ app/views.py
@@ -8,4 +8,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'Moringa School'
+    return render_template('index.html',message = message)
+
+@app.route('/pitch/<pitch_id>')
+def pitch(pitch_id):
+  '''
+  View pitch page function that returns the pitch details and its data
+  '''
+  
+  return render_template('pitch.html',id = pitch_id)
