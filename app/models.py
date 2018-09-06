95afbcf9574bdd382fc1f01d0b8f23a82f8cd88d
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-06 21:01:08 +0300

    created User class to enable creation of new users

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from . import db
+
 class Pitch:
     '''
     Pitch class to define Pitch Objects
@@ -37,3 +39,11 @@ class Review:
                 response.append(review)
 
         return response
+
+class User(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer,primary_key = True)
+  username = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'User{self.username}'
