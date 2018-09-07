d3eead111342723c04e326b27c39c9efa26a5c8f
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-07 22:50:13 +0300

    added export code for the MAIL_USERNAME and MAIL_PASSWORD

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 export SECRET_KEY='gL0711'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://gift:gL0711@localhost/pitch'
+
+export MAIL_USERNAME=giftlumumba2@gmail.com
+export MAIL_PASSWORD=gL0711419032
+
 python3.6 manage.py server
