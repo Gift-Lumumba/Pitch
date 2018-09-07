7c3364a72a60cdf1061470dcfbd951c7534b9942
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-07 22:44:44 +0300

    added upload image functionality

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 class ProdConfig(Config):
