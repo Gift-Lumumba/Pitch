ac31e26749feb01340ba65f6426ff64fe7ed8ff9
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-07 20:20:00 +0300

    Imported abort function that stops a request, and returns a response according to the status code passed in

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
+from ..models import User,Pitch,Comment,Upvote,Downvote
 
 # Views
 @main.route('/')
@@ -11,6 +12,15 @@ def index():
     '''
     title = 'Welcome to the best pitching website'
     return render_template('index.html',title = title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html",user = user)
 
 # # @main.route('/pitchs/new/',methods =['GET','POST'])
 # # @login_required
