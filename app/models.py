9cddfa44486c800fd0473f4c6cadb83e2d209049
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-07 12:22:20 +0300

    Added email column to User class,made Fourth Migration and upgrade

--- app/models.py
+++ app/models.py
@@ -3,11 +3,12 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
 
     @property
