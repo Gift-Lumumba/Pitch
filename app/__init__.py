ea87b25ed275ede978b8534010c72332f3863ced
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-09 18:53:58 +0300

    initialized flask-simplemde markdown2 extension to enable markdown display on page

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_simplemde import SimpleMDE
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -14,6 +15,7 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 mail = Mail()
+simple = SimpleMDE()
 
 photos = UploadSet('photos',IMAGES)
 def create_app(config_name):
@@ -30,6 +32,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    simple.init_app(app)
 
     # Configure UploadSet
     configure_uploads(app,photos)
