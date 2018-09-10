f92a6636e4e791d27457f319ffabf7b27dd13c66
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-10 16:29:05 +0300

    fixes email.py

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export SECRET_KEY='gL0711'
 export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://gift:gL0711@localhost/pitch'
 
-export MAIL_USERNAME='giftlumumba2@gmail.com'
-export MAIL_PASSWORD='gL0711419032'
+export MAIL_USERNAME='lumumbagift2@gmail.com'
+export MAIL_PASSWORD='gL1234567'
 
 python3.6 manage.py server
